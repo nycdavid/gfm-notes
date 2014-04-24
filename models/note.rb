fb655360aa0dcfaff78d297214527b0b81be1b3a
David Ko <david@jinglepunks.com>
2014-04-24 12:03:03 -0400

    Started moving generate preview method to own class

--- models/note.rb
+++ models/note.rb
@@ -1,3 +1,10 @@
 class Note
+  def initialize
+  end
 
+  def generate_preview(notebook, note)
+    note_path = "md-notes/notebooks/#{notebook}/#{note}.md"
+    preview = GithubMarkdownPreview::HtmlPreview.new(note_path)
+    File.read(preview.preview_file)
+  end
 end
