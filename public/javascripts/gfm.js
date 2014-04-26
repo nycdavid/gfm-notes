dcb0b820726ca3b345fd230641d7403cdd10b468
David Ko <david.ko@damncarousel.com>
2014-04-26 05:08:22 -0400

    Completed tasks should go to the bottom of the list

--- public/javascripts/gfm.js
+++ public/javascripts/gfm.js
@@ -2,4 +2,12 @@ $(document).ready(function() {
   $('h2').click(function() {
     $(this).nextAll('ul:first').slideToggle(); 
   });
+
+  // Completed tasks should go to the bottom of the list
+  $('del').each(function(i) {
+    var parent_ul = $(this).closest('ul');
+    var li = $(this).parent('li');
+
+    $(parent_ul).append($(li).detach());
+  });
 });
